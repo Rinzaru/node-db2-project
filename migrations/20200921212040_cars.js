bff382d5013afe9c8609790c53960f9b65967d1a
unknown <rinzaru@gmail.com>
2020-09-22 20:13:14 -0600

    finished hw

--- migrations/20200921212040_cars.js
+++ migrations/20200921212040_cars.js
@@ -1,7 +1,7 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("car-dealer", (table) => {
     table.integer("id").notNull().unique().primary(); //can also use table.increments("id")
-    table.text("VIN").notNull().unique();
+    table.text("vin").notNull().unique();
     table.text("make").notNull();
     table.text("model").notNull();
     table.integer("mileage").notNull();
